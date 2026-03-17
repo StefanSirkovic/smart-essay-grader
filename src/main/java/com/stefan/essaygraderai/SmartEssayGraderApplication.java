e3f2d9af66f190da840618e451fc1b228a4d69f2
Stefan Sirkovic <stefansirkovic@gmail.com>
2026-03-17 22:01:17 +0100

    Initial commit

---
+++ src/main/java/com/stefan/essaygraderai/SmartEssayGraderApplication.java
@@ -0,0 +1,13 @@
+package com.stefan.essaygraderai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEssayGraderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartEssayGraderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stefan/essaygraderai/SmartEssayGraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stefan.essaygraderai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEssayGraderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
