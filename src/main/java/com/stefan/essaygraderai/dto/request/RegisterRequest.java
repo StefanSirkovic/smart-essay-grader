a438901da3917eef2df74bc39dcfa9d2ddaf6dac
Stefan Sirkovic <stefansirkovic@gmail.com>
2026-03-24 22:21:34 +0100

    Add request for registration

--- src/main/java/com/stefan/essaygraderai/dto/request/RegisterRequest.java
+++ src/main/java/com/stefan/essaygraderai/dto/request/RegisterRequest.java
@@ -1,4 +1,24 @@
 package com.stefan.essaygraderai.dto.request;
 
-public class RegisterRequest {
-}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest(
+        @Size(min=2, max=50)
+        @NotBlank(message = "First name is required")
+        String firstName,
+
+        @Size(min=2, max=50)
+        @NotBlank(message = "Last name is required")
+        String lastName,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 8, message = "Password must be at least 8 characters")
+        String password) {}
+
+
