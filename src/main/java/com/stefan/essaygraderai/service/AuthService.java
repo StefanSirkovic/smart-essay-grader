fcff25c37d186c52ea9946f8ea606fbf2ac1a517
Stefan Sirkovic <stefansirkovic@gmail.com>
2026-04-03 14:31:08 +0200

    Add business logic for login

--- src/main/java/com/stefan/essaygraderai/service/AuthService.java
+++ src/main/java/com/stefan/essaygraderai/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.stefan.essaygraderai.service;
 
+import com.stefan.essaygraderai.dto.request.LoginRequest;
 import com.stefan.essaygraderai.dto.request.RegisterRequest;
 import com.stefan.essaygraderai.dto.response.AuthResponse;
 import com.stefan.essaygraderai.entity.User;
@@ -8,6 +9,11 @@ import com.stefan.essaygraderai.exception.EmailAlreadyExistsException;
 import com.stefan.essaygraderai.repository.UserRepository;
 import com.stefan.essaygraderai.security.JwtService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,11 +23,13 @@ public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
-    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService) {
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
     }
 
     public ResponseEntity<AuthResponse> register(RegisterRequest registerRequest) {
@@ -46,5 +54,20 @@ public class AuthService {
 
         return ResponseEntity.ok(authResponse);
 
+    }
+
+    public ResponseEntity<AuthResponse> login(LoginRequest loginRequest) {
+        String email = loginRequest.email();
+        String password = loginRequest.password();
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        String token = jwtService.generateToken(user);
+
+        AuthResponse authResponse = new AuthResponse(token, user.getId(), user.getEmail(), user.getRole());
+
+        return ResponseEntity.ok(authResponse);
     }
 }
