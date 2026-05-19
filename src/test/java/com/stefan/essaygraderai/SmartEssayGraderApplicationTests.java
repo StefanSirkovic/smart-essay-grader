ae1093d35d38d54096b8e2b8ce7cfab637170ecd
Stefan Sirkovic <stefansirkovic@gmail.com>
2026-05-19 20:24:41 +0200

    Add properties for test environment

--- src/test/java/com/stefan/essaygraderai/SmartEssayGraderApplicationTests.java
+++ src/test/java/com/stefan/essaygraderai/SmartEssayGraderApplicationTests.java
@@ -2,8 +2,10 @@ package com.stefan.essaygraderai;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SmartEssayGraderApplicationTests {
 
     @Test
