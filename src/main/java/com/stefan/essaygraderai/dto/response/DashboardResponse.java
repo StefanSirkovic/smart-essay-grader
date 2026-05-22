09ce084209ef6f242bc5dd677660fade0297713a
Stefan Sirkovic <stefansirkovic@gmail.com>
2026-05-22 13:46:32 +0200

    Add Dashboard response dto

---
+++ src/main/java/com/stefan/essaygraderai/dto/response/DashboardResponse.java
@@ -0,0 +1,11 @@
+package com.stefan.essaygraderai.dto.response;
+
+import com.stefan.essaygraderai.enums.EssayStatus;
+
+import java.util.List;
+import java.util.Map;
+
+public record DashboardResponse(long totalEssays, Map<EssayStatus, Long> essaysByStatus,
+                                Double averageScore, Double highestScore, Double lowestScore,
+                                List<EssayResponse> recentEssays) {
+}
