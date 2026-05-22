49c0d0903a144237a9ef36895d390d8884d012c0
Stefan Sirkovic <stefansirkovic@gmail.com>
2026-05-22 14:19:52 +0200

    Add Serializable implement into Dashboard response

--- src/main/java/com/stefan/essaygraderai/dto/response/DashboardResponse.java
+++ src/main/java/com/stefan/essaygraderai/dto/response/DashboardResponse.java
@@ -2,10 +2,11 @@ package com.stefan.essaygraderai.dto.response;
 
 import com.stefan.essaygraderai.enums.EssayStatus;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
 public record DashboardResponse(long totalEssays, Map<EssayStatus, Long> essaysByStatus,
                                 Double averageScore, Double highestScore, Double lowestScore,
-                                List<EssayResponse> recentEssays) {
+                                List<EssayResponse> recentEssays) implements Serializable {
 }
